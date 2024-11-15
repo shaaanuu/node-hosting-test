4eebd022f4e31a8d2f2ce029889f95d5a53c05a2
shaaanuu <monshan50@gmail.com>
2024-11-15 16:41:09 +0530

    something

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).send("Hello World!");
-}
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
